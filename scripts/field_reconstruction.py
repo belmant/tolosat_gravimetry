386b75ebf0e7f1663f8307c506a52660d2f8051b
belmant <cedric.bel@hotmail.fr>
2020-03-18 19:43:46 +0100

    Added scripts example

---
+++ scripts/field_reconstruction.py
@@ -0,0 +1,9 @@
+"""This script reconstructs the gravity field, from spherical harmonics coefficients
+
+"""
+
+def main(): # do something useful
+    pass
+
+if __name__ == '__main__': # runs only if this script is run directly, not if it is imported
+    main()
